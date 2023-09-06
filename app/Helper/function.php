fcf2d97522b63b7e79088d3bdc3ac8e6e1831499
vgoer <hi_goer@163.com>
2023-09-06 14:19:01 +0800

    init:功能函数

---
+++ app/Helper/function.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * 成功打印的函数
+ *
+ * @param  array  $data 数据
+ * @param  int  $code 状态码
+ * @param  string  $msg   返回提示
+ * @param  int  $count 获取次数
+ * @return json 返回json
+ */
+function success($data = [], $code = 200, $msg = '', $count = 0)
+{
+    return json_encode([
+        'code' => $code,
+        'msg' => $msg == '' ? '操作成功' : $msg,
+        'count' => $count == 0 ? count($data) : $count,
+        'data' => $data,
+    ]);
+}
+
+/**
+ * 返回错误信息
+ *
+ * @param  int  $code 错误码
+ * @return void|json $res_data 返回json
+ */
+function error($code = null)
+{
+    $error_code = [
+        // 系统
+        50000 => '系统繁忙',
+
+        // 登录返回
+        50100 => '账号不存在',
+
+        // 等等
+    ];
+
+    if (empty($code)) {
+        return false;
+    }
+
+    $res_json = [
+        'code' => $code,
+        'msg' => $error_code[$code],
+    ];
+
+    return json_encode($res_json);
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list', function () {
+    return success(['ys' => '111'], 200);
+});
